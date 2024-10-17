89d2e2cfd7dfc570335a36522440535853cea976
Karina <thealisky@gmail.com>
2024-10-17 23:29:24 +0300

    G3.5: enforce PR merge only if unit test coverage is above 80%

---
+++ src/sum.js
@@ -0,0 +1,29 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function stepen(a, b) {
+    return a ** b;
+}
+
+function chto_to(a, b) {
+    return a + b - b;
+}
+
+function kto_to(a, b) {
+    return a - b - b;
+}
+
+module.exports = sum;
