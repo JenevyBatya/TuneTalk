4a29e9db07473ee2c038951eb70eb8b7e25cbfee
алина <agnistova1@gmail.com>
2024-11-18 03:22:42 +0300

    fixed: headers, borders, routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Container } from '@mui/material';
 import LandingPage from './pages/LandingPage';
 import RegPage from './pages/RegPage';
 import LoginPage from "./pages/LoginPage";
@@ -11,16 +10,14 @@ import ProfilePage from "./pages/profile";
 export function App() {
     return (
         <Router>
-            <Container>
                 <Routes>
                     <Route path='/' element={<LandingPage />} />
-                    <Route path='/Registration' element={<RegPage />} />
-                    <Route path='Login' element={<LoginPage/>}/>
+                    <Route path='/registration' element={<RegPage />} />
+                    <Route path='login' element={<LoginPage/>}/>
                     <Route path="/library" element={<Library />} />
                     <Route path="/subscriptions" element={<Subscriptions />} />
                     <Route path="/account" element={<ProfilePage />} />
                 </Routes>
-            </Container>
         </Router>
     );
 }
