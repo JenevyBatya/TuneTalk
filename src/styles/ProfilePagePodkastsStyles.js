f3fe8f437a912a803fb43d17e7a2455e31b4cb09
Alohhhis <alyona@niuitmo.ru>
2024-12-05 02:40:47 +0300

    created construction

---
+++ src/styles/ProfilePagePodkastsStyles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div `
+    width: 100%;
+    max-width: 600px;
+    display: flex;
+    padding: 5%;
+`
+export const ProfileContainer = styled.div `
+    width: 100%;
+`
+export const Pic = styled.img `
+    width: auto;
+    height: auto;
+    border: 0;
+    border-radius: 20px;
+`
+export const InfoDiv = styled.div `
+    flex-direction: column;
+    text-align: left;
+`
+export const Username = styled.p `
+    font-size: 24px;
+`
+export const Description = styled.p `
+    font-size: 14px;
+`
+export const  LinkDiv= styled.div`
+
+`
+export const LinkLogo = styled.img`
+
+`
+export const Link = styled.link`
+    font-size: 10px;
+`
+export const EditButtonDiv =styled.div `
+`
+export const Black = styled.div `
+background: #173E47;
+`
+export const EditButton = styled.button `
+background: white;
+    border-radius: 10px;
+`
