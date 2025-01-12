494fcb99640aaab5f0b99653eac9616541ab323e
Karina <thealisky@gmail.com>
2025-01-12 21:22:32 +0300

    tests for HeaderComponent

---
+++ src/components/HeaderComponent.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import MobileHeader from './HeaderComponent';
+
+describe('MobileHeader Component', () => {
+    test('renders the header with a logo', () => {
+        render(
+            <MemoryRouter>
+                <MobileHeader />
+            </MemoryRouter>
+        );
+
+        // Проверяем наличие логотипа
+        const logo = screen.getByAltText('Логотип');
+        expect(logo).toBeInTheDocument();
+        expect(logo).toHaveAttribute('src', expect.stringContaining('Logo.svg')); // Проверяем путь к изображению
+    });
+
+    test('header has correct styles', () => {
+        render(
+            <MemoryRouter>
+                <MobileHeader />
+            </MemoryRouter>
+        );
+
+        const header = screen.getByRole('banner'); // Проверяем наличие header
+        expect(header).toHaveStyle('background-color: #AECBC9');
+        expect(header).toHaveStyle('position: sticky');
+    });
+});
