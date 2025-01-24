207c7e6b42d30ae9f6e8680299d48080b9faba0d
Karina <thealisky@gmail.com>
2025-01-24 15:42:19 +0300

    build

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import Root from './App';
 import './fonts/static/Jost-Light.ttf';
 import Bugsnag from '@bugsnag/js';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 import register from "./registerServiceWorker";
 
 
-const { REACT_APP_BUGSNAG_API_KEY } = process.env.REACT_APP_BUGSNAG_API_KEY;
+const {REACT_APP_BUGSNAG_API_KEY} = process.env.REACT_APP_BUGSNAG_API_KEY;
 
 Bugsnag.start({
     apiKey: process.env.REACT_APP_BUGSNAG_API_KEY,
@@ -27,10 +27,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker
-            .register("/service-worker.js")
-            .then((registration) => {
-                console.log("Service Worker registered with scope:", registration.scope);
-            })
+            .register(`${process.env.PUBLIC_URL}/service-worker.js`).then((registration) => {
+            console.log("Service Worker registered with scope:", registration.scope);
+        })
             .catch((error) => {
                 console.error("Service Worker registration failed:", error);
             });
@@ -39,13 +38,11 @@ if ("serviceWorker" in navigator) {
 
 
 root.render(
-
     <ErrorBoundary>
         <Provider store={store}>
-            <Root />
+            <Root/>
         </Provider>
     </ErrorBoundary>
-
 );
 register();
 
